e65d9306ff1027e11864919e11c2fd85fed67dc1
Christian Felde <cfelde@cfelde.com>
2019-11-22 11:08:23 +0100

    Initial commit

---
+++ src/test/java/org/web3j/unitexample/GreeterTest.java
@@ -0,0 +1,28 @@
+package org.web3j.unitexample;
+
+import org.junit.jupiter.api.Test;
+import org.web3j.EVMTest;
+import org.web3j.NodeType;
+import org.web3j.protocol.Web3j;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@EVMTest(type = NodeType.GETH)
+public class GreeterTest {
+    @Test
+    public void helloWorldTest(Web3j web3j,
+                               TransactionManager transactionManager,
+                               ContractGasProvider gasProvider) throws Exception {
+        String expectedValue = "Hello Blockchain World!";
+
+        Greeter greeter = Greeter.deploy(web3j, transactionManager, gasProvider, expectedValue).send();
+
+        String actualValue = greeter.greet().send();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
+
+
